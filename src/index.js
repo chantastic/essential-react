535dfa918f4116de80771c72dd751950b84d6f48
bdarab <bdarabzand@gmail.com>
2020-03-08 18:54:44 +0100

    make adjustments

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-const fetchPokemon = id => 
-fetch(
+const fetchPokemon = id =>
+  fetch(
     `https://pokeapi.co/api/v2/pokemon/${id}/`
   ).then(res => res.json());
 
@@ -15,42 +15,42 @@ class Pokemon extends React.Component {
     this.state = { character: null };
   }
   componentDidMount() {
-    fetchPokemon(this.props.id).then(data => 
+    fetchPokemon(this.props.id).then(data =>
       this.setState({ character: data })
     );
   }
   UNSAFE_componentWillReceiveProps(nextProps) {
     fetchPokemon(nextProps.id).then(data =>
-       this.setState({ character: data })
+      this.setState({ character: data })
     );
   }
 
-    render() {
-      return this.state.character ? (
+  render() {
+    return this.state.character ? (
       <div>
         <h3>{this.state.character.name}</h3>
         <ul>
           {this.state.character.abilities.map(ability => (
             <li key={ability.ability.name}>
-            {ability.ability.name}
+              {ability.ability.name}
             </li>
           ))}
         </ul>
       </div>
-       ) : (
+    ) : (
         <div>Loading...</div>
-       );
-    }
+      );
   }
+}
 
-class IdPager extends React.Component{
+/*class IdPager extends React.Component {
   constructor() {
     super();
     this.state = { id: 1 };
   }
 
-  render() {     
-    return(
+  render() {
+    return (
       <div>
         <h1>{this.state.id}</h1>
         <button
@@ -63,24 +63,56 @@ class IdPager extends React.Component{
           Previous
           </button>
 
-          <button 
-            type="button" 
-            onClick={() => 
-            this.setState(prevState => ({ 
+        <button
+          type="button"
+          onClick={() =>
+            this.setState(prevState => ({
               id: prevState + 1
-            }))} 
-            >
-            Next         
+            }))}
+        >
+          Next
           </button>
 
-            <this.props.component id={this.state.id} />
+        <this.props.component id={this.state.id} />
       </div>
     );
   }
+} */
+
+class PokemonPager extends React.Component {
+  constructor() {
+    super();
+    this.state = {id: 1};
+  }
+
+  render() {
+    return(
+    <div>
+      <h1>{this.state.id}</h1>
+      <button
+        type="button"
+        onClick={() => this.setState(prevState => ({
+          id: prevState - 1
+        }))}
+      >
+        Previous
+    </button>
+    <button
+    type="button" 
+          onClick={() => this.setState(prevState => ({
+            id: prevState + 1
+          }))} 
+          >
+          Next
+    </button>
+      <Pokemon id={this.state.id} />
+    </div>
+    );
+  }
 }
-
-
-ReactDOM.render(
-  <IdPager component={Pokemon} />, 
+    
+    
+  ReactDOM.render(
+  <PokemonPager />,
   document.getElementById("root")
 );
